99d497f6e5f12824bd054be1f6e444b9d89e71be
Sandhya <sandhyaginare@gmail.com>
2023-06-18 23:20:16 +0530

    Initial commit from Create Next App

---
+++ pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const home = () => {
+  return (
+    <>
+      <div>
+        <input type="text" name="" id=""  placeholder=''/>
+      </div>
+    </>
+  )
+}
+
+export default home
